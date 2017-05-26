a005c93820d643e127a90641cb48e7c29ece3b8d
Amit Kewal <amit.kewal@peopleinteractive.in>
2017-05-26 20:11:46 +0530

    minor changes

---
+++ models/EsConnector.php
@@ -0,0 +1,23 @@
+<?php
+class EsConnector
+{
+	public function __construct()
+	{
+		//load cofig properties
+		//connect with the loaded ip + port
+	}
+	
+
+	public function fireEsQuery($es_query)
+	{
+		//fire the query to elastic and return the result
+		return $result;	
+	}
+	public function updateEsQuery($campaign_objectID)
+	{
+		//update capaigns status to ACTIVE
+		return $result;	
+	}
+
+}
+?>
